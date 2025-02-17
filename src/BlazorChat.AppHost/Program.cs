89df34804f06e3db29dc246f47c9f1319550a043
Bruno Capuano <bcapuano@gmail.com>
2025-02-17 13:24:02 -0500

    Remove Azure Application Insights integration
    DETAILS
    
    The initialization of Azure Application Insights has been removed from the builder. This includes the deletion of the line that added Application Insights with the identifier "appInsights" and the subsequent reference to `appInsights` in the project configuration. The overall application setup remains intact without Application Insights integration.

--- src/BlazorChat.AppHost/Program.cs
+++ src/BlazorChat.AppHost/Program.cs
@@ -1,9 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var appInsights = builder.AddAzureApplicationInsights("appInsights");
 
 builder.AddProject<Projects.BlazorChat_ChatApp>("chatapp")
-    .WithReference(appInsights)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
